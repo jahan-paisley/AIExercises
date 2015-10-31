427926d80c8e10286598c635ec8e61cc080449a4
Jahangir Zinedine <jzinedine@gmail.com>
2015-11-01 03:26:25 +0330

    almost completed

--- lib/exercise1.rb
+++ lib/exercise1.rb
@@ -1,12 +1,7 @@
 require "numeric"
 
 class Exercise1
-  attr_reader :popu
-
-  def self.fitness(x, y)
-    e = Math::E
-    (1-x)^2*e^((-x^2)-(y+1)^2)-(x-x^3-y^3)*e^(-x^2-y^2)
-  end
+  attr_reader :popu, :elements
 
   def initialize pm, pc, ip, itc
     @pm, @pc, @ip, @itc= pm, pc, ip, itc
@@ -14,23 +9,146 @@ class Exercise1
   end
 
   def generate
-    @popu= (0..@ip).map { Random.rand(0..255) }
-    @popu_f= @popu.map{|e| fitness(e&0xFF00, e&0x00FF)}
+    @popu= (0..@ip).map { Random.rand(0..65535) }
+    @elements= []
+    @elements= @popu.map { |el| Element.new(el) }
+    calc_f_ratio
+  end
+
+  def calc_f_ratio
+    fit_sum = @elements.reduce(0) { |sum, el| sum+=el.fitness }
+    @elements.each do |el|
+      el.f_ratio = el.fitness/fit_sum
+    end
+  end
+
+  def select_pair
+    # binding.pry
+    first = Random.rand()
+    second = Random.rand()
+    @selected ||= []
+    sorted = @elements.sort_by { |el| el.f_ratio }
+    pair1 = sorted.select { |el| el.f_ratio >= first }.first
+    if pair1.nil?
+      if first < sorted.map { |e| e.f_ratio }.min
+        pair1= sorted.first
+      elsif first > sorted.map { |e| e.f_ratio }.min
+        pair1= sorted.last
+      else
+        raise("error in f_ratio ")
+      end
+    end
+    @selected << pair1
+    sorted2 = sorted.reject { |el| @selected.include?(el) }
+    pair2 = sorted2.select { |el| el.f_ratio >= second }.first
+    if sorted2.map { |e| e.f_ratio }.min.nil?
+      return pair1, pair1
+    end
+    if pair2.nil?
+      if second < sorted2.map { |e| e.f_ratio }.min
+        pair2= sorted2.first
+      elsif second > sorted2.map { |e| e.f_ratio }.min
+        pair2= sorted2.last
+      else
+        raise("error in f_ratio ")
+      end
+    end
+
+    @selected << pair2
+    binding.pry if pair1.nil? or pair2.nil?
+    return pair1, pair2
+  end
+
+  def best_match pop
+    pop.max{|a,b| a.fitness <=> b.fitness}
+  end
+
+  def worst_match pop
+    pop.min{|a,b| a.fitness <=> b.fitness}
   end
 
   def next_generation
-    new_popu= []
-    @popu.combination(2) do |a, b|
-      child = crossover(a, b)
-      if fit?(child)
-        new_popu << child
-      else
-        new_popu.push([a, b)
-      end
+    new_popu = []
+    while new_popu.length < @ip
+      a, b = select_pair
+      child1, child2 = crossover(a, b)
+      new_popu.push(child1, child2)
     end
-    @popu=@popu.map { |e| @pm >= Random.rand() ? mutate(e) : e }
+    raise("population exceeded exptected value") if new_popu.length > @ip
+    new_popu.each_with_index { |el, i| new_popu[i]= mutate(el) if Random.rand() <= @pm }
+    n_best_match = best_match(new_popu)
+    if n_best_match.fitness < best_match(@elements).fitness
+      new_popu.reject!(worst_match(new_popu))
+      new_popu << n_best_match
+    end
+    @selected= []
+    @elements= new_popu
+  end
 
+  def run
+    @itc.times do
+      next_generation
+    end
+
+    mx = @elements.max { |a, b| a.fitness <=> b.fitness }
+    puts "maximum value is x=#{mx.x}, y= #{mx.y}, fitness= #{mx.fitness}"
+  end
+
+  def crossover a, b
+    r = Random.rand()
+    if r <= @pc
+      a1= a.elem
+      b1= b.elem
+      a.elem = b1 & 0xFF00 + a1&0x00FF
+      b.elem = a1 & 0xFF00 + b1&0x00FF
+
+      puts a.x, a.y
+      puts b.x, b.y
+    end
+    return a, b
+  end
+
+  def mutate el
+    r = Random.rand(16)
+    bin = el.elem.to_s(2).rjust(16, '0')[r]= el.elem.to_s(2).rjust(16, '0')
+    bin[r] == 0 ? '1' : '0'
+    el.elem= bin.to_i(2)
+    el
   end
 
 end
 
+#xf 101.02824551412276
+#yf 92.32139366069683
+
+class Element
+  attr_accessor :elem, :f_ratio, :x, :y
+
+  def initialize elem
+    @f_ratio = 0
+    @elem= elem
+    @x=x
+    @y=y
+  end
+
+  def self.fit(x, y)
+    e = Math::E
+    (1-x)**2*e**((-x**2)-(y+1)**2)-(x-x**3-y**3)*e**(-x**2-y**2)
+  end
+
+  def x
+    @x= (@elem & 0x00FF)*0.0235294 - 3
+  end
+
+  def y
+    @y=((@elem & 0xFF00) >> 8)*0.0235294 - 3
+  end
+
+  def fitness
+    x= @elem & 0x00FF
+    y= (@elem & 0xFF00) >> 8
+    x= x*0.0235294 - 3
+    y= y*0.0235294 - 3
+    Element.fit(x, y)
+  end
+end
