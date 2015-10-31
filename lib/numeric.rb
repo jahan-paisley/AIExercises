91456fd938dd91a491ccb5fd8a5691fc0f8786cb
Jahangir Zinedine <jzinedine@gmail.com>
2015-11-01 03:26:25 +0330

    almost completed

--- lib/numeric.rb
+++ lib/numeric.rb
@@ -1,14 +1,14 @@
-Numeric.class_eval do
-  # original_times = instance_method(:^)
-  define_method(:^) do |other|
-    return self ** other
-
-  end
-end
-
-Fixnum.class_eval do
-  # original_times = instance_method(:^)
-  define_method(:^) do |other|
-    return  self **other
-  end
-end
+# Numeric.class_eval do
+#   # original_times = instance_method(:^)
+#   define_method(:^) do |other|
+#     return self ** other
+#
+#   end
+# end
+#
+# Fixnum.class_eval do
+#   # original_times = instance_method(:^)
+#   define_method(:^) do |other|
+#     return  self **other
+#   end
+# end
